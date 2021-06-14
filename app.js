fc7d1ba3c24ee86c7a32e6c775a463451a5ed4e5
WON-JIN-LEE <vopgh0409@gmail.com>
2021-06-15 00:09:11 +0900

    ADD const , let, var

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const a = 10;
+const b = 2;
+
+let myName = "Jin";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "WonJin";
+
+console.log("hello " + myName);
