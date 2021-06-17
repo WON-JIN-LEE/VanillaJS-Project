14f0c14e1702cf72543ea38530725205d16d864c
WON-JIN-LEE <vopgh0409@gmail.com>
2021-06-17 15:57:32 +0900

    DOMTokenList Methods

--- app.js
+++ app.js
@@ -1,41 +1,7 @@
-const title = document.querySelector(".hello h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
-console.dir(title);
-
+console.log(h1);
 function handleTitleClick() {
-    title.style.color = "black";
+    h1.classList.toggle("clicked");
 }
-
-function handleMouseEnter() {
-    title.innerText = "Mouse is here";
-}
-
-
-function handleMouseLeave() {
-    title.innerText = "Mouse is gone";
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "aqua";
-}
-
-function handleWindowCopy() {
-    alert("copy paste");
-}
-
-function handleWindowOffline() {
-    alert("SoS no WIFI");
-}
-
-function handleWindowOnline() {
-    alert("Good");
-}
-
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
--- exampleCode/moreEvents.md
+++ exampleCode/moreEvents.md
@@ -59,3 +59,26 @@ window.addEventListener("copy", handleWindowCopy);
 window.addEventListener("offline", handleWindowOffline);
 window.addEventListener("online", handleWindowOnline);
 ```
+
+```javascript
+const h1 = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick() {
+  const clickedClass = "clicked";
+  if (h1.classList.contains(clickedClass)) {
+    h1.classList.remove(clickedClass);
+  } else {
+    h1.classList.add(clickedClass);
+  }
+}
+h1.addEventListener("click", handleTitleClick);
+```
+
+```javascript
+const h1 = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick() {
+  h1.classList.toggle("clicked");
+}
+h1.addEventListener("click", handleTitleClick);
+```
