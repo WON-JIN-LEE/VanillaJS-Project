3ea11361157382788918e1b522b44c946a6744ea
WON-JIN-LEE <vopgh0409@gmail.com>
2021-06-19 17:13:52 +0900

    Add UserName Save

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
+
 function onLoginSubmit(event) {
     event.preventDefault();
-    console.log(loginInput.value);
+    const username = loginInput.value;
+    
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    
+    localStorage.setItem("username", username);
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello! ${username}`;
 
 }
 
