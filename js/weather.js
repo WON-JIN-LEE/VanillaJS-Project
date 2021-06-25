d57550b4b72f26a9ab52b5818c22eb6dbecc6917
WON-JIN-LEE <vopgh0409@gmail.com>
2021-06-26 00:47:42 +0900

    CSS : Update clock, toDoList-Box, quote-style

--- js/weather.js
+++ js/weather.js
@@ -8,12 +8,12 @@ function onGeoOk(position) {
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     console.log(url);
     fetch(url).then(response => response.json()).then(data => {
-        const weather = document.querySelector("#weather span:nth-child(2)");
-        const temp = document.querySelector("#weather span:nth-child(4)");
-        const city = document.querySelector("#weather span:last-child");
+        const weather = document.querySelector("#weather :nth-child(2)");
+        const temp = document.querySelector("#weather :nth-child(4)");
+        const city = document.querySelector("#weather :last-child");
         city.innerText = data.name;
         weather.innerText = `${data.weather[0].main} / `;
-        temp.innerText = ` ${data.main.temp}`
+        temp.innerText = `${data.main.temp}`
         selectedIcon(data.weather[0].main);
     });
 }
