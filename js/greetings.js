88f7db40d872f0d5278ad039244f0b35ad12ffaf
WON-JIN-LEE <vopgh0409@gmail.com>
2021-06-27 23:00:17 +0900

    CSS :  login__container, clock__container  Add  Animation

--- js/greetings.js
+++ js/greetings.js
@@ -6,7 +6,8 @@ const greeting = document.querySelector("#greeting");
 const clockHidden = document.querySelector("#clock");
 const toDoFormHidden = document.querySelector("#todo-form");
 const todoContainer = document.querySelector("#todo-container");
-const home= document.querySelector("#home");
+const loginContainer = document.querySelector(".login__container");
+const clockContainer = document.querySelector(".clock__container");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -14,25 +15,34 @@ const USERNAME_KEY = "username";
 function onLoginSubmit(event) {
     event.preventDefault();
     const username = loginInput.value;
-    loginForm.classList.add(HIDDEN_CLASSNAME);
     localStorage.setItem(USERNAME_KEY, username);
+
     paintGreeting(username);
 }
 
 function paintGreeting(username) {
-    greeting.innerText = `Hello! ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-    clockHidden.classList.remove(HIDDEN_CLASSNAME);
-    toDoFormHidden.classList.remove(HIDDEN_CLASSNAME);
-    todoContainer.classList.remove(HIDDEN_CLASSNAME);
+    loginContainer.classList.add('anim-out');
+    clockContainer.classList.add('anim-out');
+    setTimeout(() => {
+        loginForm.classList.add(HIDDEN_CLASSNAME);
+        greeting.innerText = `Good morning! ${username}`;
+        greeting.classList.remove(HIDDEN_CLASSNAME);
+        clockHidden.classList.remove(HIDDEN_CLASSNAME);
+        toDoFormHidden.classList.remove(HIDDEN_CLASSNAME);
+        todoContainer.classList.remove(HIDDEN_CLASSNAME);
+
+        clockContainer.classList.remove('anim-out');
+
+    }, 300)
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
     loginForm.classList.remove("hidden");
+    loginContainer.classList.remove('anim-out');
     loginForm.addEventListener("submit", onLoginSubmit);
+
 } else {
     paintGreeting(savedUsername);
 }
-
