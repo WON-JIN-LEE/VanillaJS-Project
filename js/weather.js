857ada6640fc6abc9d366b96dd4145c77a393a2e
WON-JIN-LEE <vopgh0409@gmail.com>
2021-07-28 22:45:49 +0900

    Update weather

--- js/weather.js
+++ js/weather.js
@@ -6,11 +6,11 @@ function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    console.log(url);
     fetch(url).then(response => response.json()).then(data => {
         const weather = document.querySelector("#weather :nth-child(2)");
         const temp = document.querySelector("#weather :nth-child(4)");
         const city = document.querySelector("#weather :last-child");
+        console.log(data);
         city.innerText = data.name;
         weather.innerText = `${data.weather[0].main} / `;
         temp.innerText = `${data.main.temp}`
